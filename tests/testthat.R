801b73e29b67c017dd15963b022569c96a222b58
Janne Huovari <janne.huovari@ptt.fi>
2019-10-02 09:36:18 +0300

    tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(uljas)
+
+test_check("uljas")
---
+++ tests/testthat/test-get.R
@@ -0,0 +1,29 @@
+
+context("Get table of statistics")
+
+test_that("uljas_stats works", {
+  test <- uljas_stats(lang = "en")
+  expect_equal(names(test), c("ifile", "title", "utime"))
+})
+
+context("Get dimensions")
+
+test_that("uljas_dims works", {
+  test <- uljas_dims(ifile = "/DATABASE/01 ULKOMAANKAUPPATILASTOT/02 SITC/ULJAS_SITC")
+  expect_equal(names(test[[1]]), c("label", "elim",  "size",  "show"))
+})
+
+context("Get classifications")
+
+test_that("uljas_class works", {
+  test <- uljas_class(ifile = "/DATABASE/01 ULKOMAANKAUPPATILASTOT/02 SITC/ULJAS_SITC", class = "SITC Products")
+  expect_equal(names(test[[1]]), c("code", "text"))
+})
+
+context("Get data")
+
+test_that("uljas_class works", {
+  sitc_query <- list(`SITC Products` = c("02"), TimePeriod = "=LAST", Flow = 1, Country = "AT", Indicators = "V1")
+  test <- uljas_data(ifile = "/DATABASE/01 ULKOMAANKAUPPATILASTOT/02 SITC/ULJAS_SITC", classifiers = sitc_query)
+  expect_equal(names(test), c(names(sitc_query)[1:4], "values"))
+})
